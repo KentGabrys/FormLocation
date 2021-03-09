42fc2831411cab5171a5b8ab6af4cdcce9830703
KentGabrys <Abyss@codecruncher.com>
2021-03-09 18:08:30 -0500

    Add setup for recorder tests

--- FormLocationTests/RecorderSerializesFormElementsTests.cs
+++ FormLocationTests/RecorderSerializesFormElementsTests.cs
@@ -10,21 +10,28 @@ namespace FormLocationTests
     [TestFixture]
     public class RecorderSerializesFormElementsTests
     {
+        private WindowRecorder _recorder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _recorder = new WindowRecorder();
+            _recorder.Size = new Size( 640, 480 );
+            _recorder.Location = new Point( 200, 200 );
+            _recorder.Name = "Chester";
+            _recorder.Save();
+        }
+
 
         [Test]
         public void RecorderSavesDataTest()
         {
-            var recorder = new WindowRecorder();
-            recorder.Size = new Size( 640, 480 );
-            recorder.Location = new Point( 200, 200 );
-            recorder.Name = "Chester";
-            recorder.Save();
-
             var settingsFilePath = 
                 Path.Combine( 
                     Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData ),
                     Assembly.GetExecutingAssembly().GetName().Name,
                     "formSettings.json" );
+
             Assert.IsTrue( File.Exists( settingsFilePath ) );
 
         }
